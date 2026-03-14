e01999e6f44e628c108c0257055c70e58cdfb4cd
shaw-wu <xz_640888@163.com>
2026-03-14 11:43:40 +0800

    delete build

--- build/obj_dir/sim_main.d
+++
@@ -1,9 +0,0 @@
-sim_main.o: /home/shaw/fpu/csrc/sim_main.cpp Vtestbench.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilated_config.h \
- /usr/local/share/verilator/include/verilatedos.h \
- /usr/local/share/verilator/include/verilated_types.h \
- /usr/local/share/verilator/include/verilated_funcs.h \
- /usr/local/share/verilator/include/verilated_vcd_c.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilated_trace.h
